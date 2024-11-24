f64b1e6c75d8bac513aa54ebdfed168554052487
Olimxon <Olimxon401@gmail.com>
2024-11-25 02:03:30 +0500

    ready about

---
+++ app/about/index.jsx
@@ -0,0 +1,36 @@
+import './about.scss'
+
+export default function About() {
+    return (
+        <div className="about">
+            <h1>Why We are Best Food Maker</h1>
+            <br />
+            <p>Experience the art of culinary perfection. Our passion for cooking brings <br /> the finest flavors to your table, crafted with love and the freshest ingredients.</p>
+
+            <div>
+                <div className='text'>
+                    <p>
+                        It is a long established fact that a reader will be distracted  
+                         layout. The point of using Lorem Ipsum is that it has a more- 
+                         or-less normal distribution of letters, as opposed to using 
+                          English. Many desktop publishing packages and web page 
+                           editors now use Lorem Ipsum as their default model text, 
+                            and a search for 'lorem ipsum' will uncover many web sites 
+                             still in their infancy. Various versions have evolved over the 
+                              years, sometimes by accident, sometimes on purpose It is a 
+                               long established fact that a reader will be distracted by the 
+                                readable content of a page when looking at its layout. The
+                                 normal distribution of letters, as opposed to using 'Content 
+                                   Many desktop publishing packages and web page editors 
+                                    search for 'lorem ipsum' will uncover many web sites still in 
+                                      humour and the like.
+                    </p>
+
+                </div>
+                <div className='video'>
+                    <img src="/images/Video-1.png" alt="" />
+                </div>
+            </div>
+        </div>
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,22 @@
+import './landing.scss'
+import About from './about'
+
 export default function Home() {
   return (
-    <>
-    </>
+    <main>
+      <div className="home">
+        <div className='title'>
+          <h1>
+            Welcome! <br />
+          We Made Delicious Food for You <br />
+          </h1>
+          <p>
+            Experience the art of culinary perfection. Our passion for cooking brings the finest <br /> flavors to your table, crafted with love and the freshest ingredients.
+          </p>
+          <button className='order'>Order Online</button>
+        </div>
+      </div>
+        < About />
+    </main>
   );
 }
